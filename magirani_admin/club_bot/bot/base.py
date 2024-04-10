c608fbcd38a21d3caec4fe9c530242ca5cdcfbff
dgush <dgushch@gmail.com>
2024-04-10 14:43:56 +0400

    1.1.9

--- magirani_admin/club_bot/bot/base.py
+++ magirani_admin/club_bot/bot/base.py
@@ -11,19 +11,23 @@ bot = telebot.TeleBot(BOT_TOKEN, parse_mode='html')
 
 
 def log_error(message, with_traceback: bool = True):
-    now = datetime.now(TZ)
-    log_folder = now.strftime ('%m-%Y')
-    log_path = os.path.join(BASE_DIR, 'club_bot', 'bot', 'bot_log', log_folder)
+    try:
+        now = datetime.now(TZ)
+        log_folder = now.strftime ('%m-%Y')
+        log_path = os.path.join(BASE_DIR, 'club_bot', 'bot', 'bot_log', log_folder)
 
-    if not os.path.exists(log_path):
-        os.makedirs(log_path)
+        if not os.path.exists(log_path):
+            os.makedirs(log_path)
 
-    log_file_path = os.path.join(log_path, f'{now.day}.log')
-    logging.basicConfig (level=logging.WARNING, filename=log_file_path, encoding='utf-8')
-    if with_traceback:
-        ex_traceback = traceback.format_exc()
-        logging.warning(f'{now}\n{ex_traceback}\n{message}')
-    else:
-        logging.warning(f'{now}\n{message}')
+        log_file_path = os.path.join(log_path, f'{now.day}.log')
+        logging.basicConfig (level=logging.WARNING, filename=log_file_path, encoding='utf-8')
+        if with_traceback:
+            ex_traceback = traceback.format_exc()
+            logging.warning(f'{now}\n{ex_traceback}\n{message}')
+        else:
+            logging.warning(f'{now}\n{message}')
+
+    except:
+        pass
 
 
